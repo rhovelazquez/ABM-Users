1675585a85c50f5007da4010ec5780d87f0ed7d7
rhovelazquez <rocyvelazquez2021@gmail.com>
2024-10-18 00:24:25 -0300

    implementacion de logout

--- src/controller/userAuthController.js
+++ src/controller/userAuthController.js
@@ -66,3 +66,10 @@ export const login=async(req,res)=>{
         return res.status(500).json({ error: error.message });
     }
 }
+
+export const logout=(req, res)=>{
+    res.cookie("token","",{
+        expires:new Date(0)
+    })
+    return res.sendStatus(200)
+}
--- src/routes/userAuthRoutes.js
+++ src/routes/userAuthRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { register,login } from '../controller/userAuthController.js';
+import { register,login,logout} from '../controller/userAuthController.js';
 
 router.post('/register',register);
 router.post('/login',login)
-
+router.post('/logout',logout)
 export default router
